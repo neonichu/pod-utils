6ab5abe08b1387fb48c54019d225844295920f8a
Boris Bügling <boris@icculus.org>
2015-03-17 11:01:13 +0100

    Quickly check the quality metrics of a Pod.

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Some scripts to do stuff with CocoaPods.
 
 - `changelog.rb` - generate a ChangeLog file from GH releases
 - `has_tests.rb` - checks if a Pod has test targets in its Xcode project
-
+- `quality.rb` - quickly check the quality metrics of a Pod on the command line
---
+++ quality.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'net/http'
+require 'uri'
+
+if ARGV.length < 1
+	puts "Usage: #{$PROGRAM_NAME} [POD_NAME]"
+	exit 1
+end
+
+uri = URI.parse("https://cocoadocs-api-cocoapods-org.herokuapp.com/pods/#{ARGV[0]}/stats")
+quality = Net::HTTP.get_response(uri).body
+if quality[0] != '['
+	puts quality
+	exit 1
+end
+quality = JSON.parse(quality)
+
+uri = URI.parse("http://metrics.cocoapods.org/api/v1/pods/#{ARGV[0]}.json")
+metrics = JSON.parse(Net::HTTP.get_response(uri).body)
+
+quality.each do |metric|
+	if (metric['modifier'] > 0 && !metric['applies_for_pod']) ||
+	   (metric['modifier'] < 0 && metric['applies_for_pod'])
+		puts "🚫 #{metric['title']}: #{metric['description']}"
+	end
+end
+
+puts "\nCurrent quality estimate: #{metrics['cocoadocs']['quality_estimate']}"
